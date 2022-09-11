f4894cfd76c98324f12ef7534844d53ea1a601c7
clasernaj <clasernaj@gmail.com>
2022-09-10 21:10:59 -0700

    finish proj_1

--- CSCI40/project_1/proj_1.cpp
+++ CSCI40/project_1/proj_1.cpp
@@ -1,8 +1,3 @@
-// libraries
-#include <iostream>
-#include <stdlib.h>
-#include <time.h>
-
 // headers
 #include "proj_1.h"
 
@@ -10,35 +5,138 @@ using namespace std;
 
 int main()
 {
-    // initialize player and seed random
-    string playerChoice;
+    // initialize vars
     playerStats player;
-    srand(time(NULL));
-    
+    int dice, playerAttempts;
+
+    // I didn't loop through inputs to make it capitalized,
+    // but I made the function for it and tested it.
+    // cout << player.chooseOptLoop(); 
+
+
+    // GAME ROLL STATS
     do
     {
-        // this will repeat as long as the user rerolls
         player.Generate();
-        player.Print();
+        player.PrintAll();
 
         cout << "These are your stats"
                 "\nWhat would you like to do?"
                 "\nBEGIN, REROLL, or ANY to QUIT"
              << endl;
-        cin >> playerChoice;
-    } while (playerChoice == "REROLL");
+
+        player.chooseOpt();
+
+    } while (player.Choice == "REROLL");
 
     // quit program on ANY key
-    if (playerChoice != "BEGIN")
+    if (player.Choice != "BEGIN")
     {
         return 0;
     }
 
-    // start of game
-    cout << "You are standing in front of the gate to a large mysterious mansion.The gates rusted lock seems ready to fall off."
-            "\n\nMaybe if you bashedmk the gate hard enough, you could get through it. Or, you could try to climb over the gate."
-            "\n\nWould you like to CLIMB or BASH the gate? "
+    // GAME START
+    cout << string(20, '-') << "\nYou are standing in front of the gate to a large mysterious mansion.The gates rusted lock seems ready to fall off."
+                               "\n\nMaybe if you bashed the gate hard enough, you could get through it. Or, you could try to climb over the gate."
          << endl;
 
-    cin >> playerChoice;
+    playerAttempts = 0;
+
+    do {
+        cout << "\nWould you like to CLIMB or BASH the gate? ";
+        player.chooseOpt();
+
+        if (player.Choice != "CLIMB" && player.Choice != "BASH"){
+            playerAttempts += 1;
+            cout << "Attempt " << playerAttempts << ": I'm sorry I did not understand that response. Please try again." << endl;
+        } else {
+            break;
+        }
+    } while (playerAttempts < 3);
+
+    if (player.Choice == "CLIMB")
+    {
+        dice = player.rollDie(1, 20);
+        cout << "[ You rolled a " << dice << " against a Dex of " << player.Dexterity << " ]" << endl;
+
+        if (dice <= player.Dexterity)
+        {
+            cout << string(20, '-')
+                 << "\nSuccess."
+                 << "\nYou carefully climb over the old rusted gate."
+                 << "\n\nYou land safely on your feet inside the grounds of the mysterious mansion."
+                 << endl;
+        }
+        else
+        {
+            dice = player.rollDie(1, 4);
+            player.CurrHealth -= dice;
+            player.PrintHealth();
+
+            cout << "Fail."
+                 << "\nAs you make your way over the rusted gate, the finial you are using for support snaps and you land hard on the ground below."
+                 << "\n\nYou take ["
+                 << dice
+                 << "] damage and are a little shaken. " << endl;
+
+            if (player.CurrHealth <= 0)
+            {
+                cout << "You Fainted From Exhaustion." << endl;
+                return 0;
+            }
+            else
+            {
+                cout
+                    << "\n\nNevertheless, you have made it inside the grounds of the mysterious mansion."
+                    << endl;
+            }
+        }
+    }
+    else if (player.Choice == "BASH")
+    {
+        dice = player.rollDie(1, 20);
+        cout << "[ You rolled a " << dice << " against a Str of " << player.Strength << " ]" << endl;
+
+        if (dice <= player.Strength)
+        {
+            cout << string(20, '-')
+                 << "\nSuccess."
+                 << "\nYou put all your weight into a massive assault on the gate."
+                 << "\nAs you suspected, the lock was not up to the challenge and gave way easily."
+                 << "\nYou hear a large thud as the gate crashes open."
+                 << "\n\nYou have made it inside the grounds of the mysterious mansion."
+                 << endl;
+        }
+        else
+        {
+            dice = player.rollDie(1, 4);
+            player.CurrHealth -= dice;
+            player.PrintHealth();
+
+            cout << "You put all your weight into a massive assault on the gate."
+                 << "\nAt first, the lock resists your attack, and the collision with the gate bites into your shoulder."
+                 << "\n\nYou take ["
+                 << dice
+                 << "] damage." << endl;
+
+            if (player.CurrHealth <= 0)
+            {
+                cout << "You Fainted From Exhaustion." << endl;
+                return 0;
+            }
+            else
+            {
+                cout
+                    << "\n\nAfter a moment's hesitation, the lock gives, and the gate swings open with a thud. You have made it inside the grounds of the mysterious mansion."
+                    << endl;
+            }
+        }
+    }
+
+    // GAME END
+    cout << string(20, '_')
+         << "\nThank you for playing our demo. We hope you have enjoyed it." << endl;
+    
+    cin >> player.Choice;
+    return 0;
 };
--- CSCI40/project_1/proj_1.h
+++ CSCI40/project_1/proj_1.h
@@ -1,6 +1,7 @@
 #include <algorithm> // for any_of
-#include <string>    // for easier string manip.
+#include <string>    // for string manip.
 #include <iostream>
+#include <cctype>
 #include <stdlib.h>
 #include <time.h>
 
@@ -8,13 +9,17 @@ using namespace std;
 
 class playerStats
 {
+    // variables
 public:
     int Strength, Dexterity, Intelligence, Charisma, MaxHealth, CurrHealth;
+    string Choice;
 
 public:
     void Generate()
     {
         // Roll or re-rolls the stats
+        // Doesn't take any arguments
+
         srand(time(NULL));
 
         // all 3-18
@@ -28,21 +33,24 @@ public:
         CurrHealth = MaxHealth;
     };
 
+    // Stat Printing Functions
 public:
-    void Print()
+    void PrintAll()
     {
-        // prints current stats in it's current state
-        // strength, dexterity, intelligence, charisma, maxhealth and curr health needs to be defined
+        // Prints current stats in it's current state
 
-        int statsArr[5] = {Strength, Dexterity, Intelligence, Charisma, MaxHealth}; // used for fail-safe
-        int healthBarPercent = (CurrHealth / MaxHealth) * 10;                       // Ex: 2/5 is 0.4 or 40% of the bar (4/10)
+        // used for fail-safe
+        int statsArr[5] = {Strength, Dexterity, Intelligence, Charisma, MaxHealth};
+
+        // Ex: 2/5 is 0.4 or 40% of the bar (4/10)
+        int healthBarPercent = ((float)CurrHealth / (float)MaxHealth) * 10;
 
         // any_of will check if any of the stats are 0 (which they shouldn't be)
         any_of(statsArr, statsArr + 5, [](int x)
                { return x == 0; })
             ? cout << "An Error Occured. Stats are not initialized" << endl
             :
-            // print ascii stats table
+            // ascii stats table
             cout
                 << string(20, '_') << "\n"
                 << " : Strength: " << Strength
@@ -55,5 +63,57 @@ public:
                 << "\n"
                 << string(20, '_') << "\n"
                 << endl;
+    }
+
+public:
+    void PrintHealth()
+    {
+        // Ex: 2/5 is 0.4 or 40% of the bar (4/10)
+        int healthBarPercent = ((float)CurrHealth / (float)MaxHealth) * 10;
+
+        // I found that the program breaks if I do string(-int, char)
+        // So health = 0 if dmg > health so that it won't break.
+        if (healthBarPercent < 0) healthBarPercent = 0;
+    
+        // ascii health bar
+        cout
+            << string(20, '_') << "\n"
+            << ": Health"
+            << "(" << CurrHealth << "/" << MaxHealth << "): "
+            << string(healthBarPercent, (char)254u) << string((10 - healthBarPercent), 'x')
+            << "\n"
+            << string(20, '_') << "\n"
+            << endl;
+    }
+
+    // general functions
+public:
+    int rollDie(int rangeStart, int rangeEnd)
+    {
+        // picks a number ranging from rangeStart to rangeEnd
+        return ((rand() % rangeEnd) + rangeStart);
+    }
+
+public:
+    string chooseOpt()
+    {
+        // Lowercase input -> Uppercase so that it's accepted.
+        cin >> Choice;
+        transform(Choice.begin(), Choice.end(), Choice.begin(), ::toupper);
+
+        return Choice;
+    }
+
+public:
+    string chooseOptLoop() {
+        // This is just for the assignment
+        // if I were to loop through the string it would look like this
+        cin >> Choice;
+
+        for (int i = 0; i < Choice.size(); ++i) {
+            Choice[i] = toupper(Choice[i]);
+        }
+
+        return Choice;
     }
 };
